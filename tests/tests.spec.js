2afb7ce1e8f97bfc6b25828ab178c275e74a99c0
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-18 15:12:17 -0500

    added imports to jest test script

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,1 +1,5 @@
 // write your tests here
+// import React, { useState } from "react";
+// import { render, unmountComponentAtNode } from "react-dom";
+// import { create, act } from "react-test-renderer";
+// import { act } from "react-dom/test-utils";
